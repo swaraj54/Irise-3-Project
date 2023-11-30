c00496f53f4a0656c787c791948a6983826b1d69
Swaraj Jadhav <70018714+swaraj54@users.noreply.github.com>
2023-11-30 09:31:11 +0530

    Updates

--- test.js
+++ test.js
@@ -1,3 +1,26 @@
-console.log(2 == "2") //  checks only data
+// console.log(2 == "2") //  checks only data
 
-console.log(2 === "2") // check data and datatype
+// console.log(2 === "2") // check data and datatype
+
+
+// ;
+// const response = await axios.get("/products")
+
+// { data: { message: "Porducts fetched successfully.", products : products } }
+
+// console.log(response.data.message)
+
+
+// const { data } = await axios.get("/products")
+// console.log(data.message)
+
+const myObject = { myname: "irise", myAge: 5 };
+
+console.log(myObject.myname)
+
+const { myname, myAge } = myObject;
+
+console.log(myname)
+console.log(myAge)
+
+
